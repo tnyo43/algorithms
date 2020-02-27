506655cb7ca3b1460a44ab297d4318a3e6547959
tyno43 <tnyo43@gmail.com>
2020-02-27 15:12:02 +0900

    add modint

--- math/modint.cpp
+++ math/modint.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-#define FOR(i,m,n) for (ll i = m; i < n; ++i)
-
 template<int mod> struct ModInt {
     int64_t x;
     ModInt() : x(0) {}
@@ -83,46 +80,11 @@ template<int mod> struct ModInt {
     }
 };
 
-// const int MOD = 998244353;
-const int MOD = 1e9+7;
+const int MOD = 998244353;
+// const int MOD = 1e9+7;
+
 using modint = ModInt<MOD>;
 
-#define FOR(i,m,n) for (ll i = m; i < n; ++i)
-
-const int MAX = 1e5+10;
-
-modint fac[MAX<<2];
-modint finv[MAX<<2];
-modint inv_[MAX<<2];
-
-void init() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv_[1] = 1;
-    FOR (i, 2, MAX<<2) {
-        fac[i] = fac[i-1]*modint(i);
-        inv_[i] = -inv_[MOD%i]*modint(MOD/i);
-        finv[i] = finv[i-1]*inv_[i];
-    }
-}
-
-modint nCk (int n, int k) {
-    if (n < k || k < 0 || n < 0) return 0;
-    return fac[n]*finv[k]*finv[n-k];
-}
-
-modint comb(int n, int k) {
-    modint ret(1);
-    FOR (i, 1, k+1) ret *= i;
-    ret = ret.inverse();
-    FOR (i, n-k+1, n+1) ret *= i;
-    return ret;
-}
-
-modint nHk (int n, int k) {
-    return nCk(n-1+k, n-1);
-}
-
 int main() {
-    init();
+    
 }
