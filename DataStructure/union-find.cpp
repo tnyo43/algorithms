11282589a343f4fa2557dbc9ad4ee3365882857d
tyno43 <tnyo43@gmail.com>
2020-04-12 18:55:42 +0900

    add union find module

--- DataStructure/union-find.cpp
+++ DataStructure/union-find.cpp
@@ -1,57 +1,45 @@
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
-
+typedef vector<ll> vi;
 #define REP(i,n) for (ll i = 0; i < n; ++i)
 
-const int MAX = 2e5;
+struct UnionFind {
+private:
+    int N;
+    vi parent;
+    vi rank_;
 
-int parent[MAX];
-int rankb[MAX];
- 
-void init(int x) {
-    for(int i = 0; i <= x; i++)
-    parent[i] = i,rankb[i] = 0;
-}
- 
-int root(int x) {
-    return (parent[x]==x) ? x : parent[x]=root(parent[x]);
-}
- 
-bool same(int x, int y) {
-    return root(x) == root(y);
-}
- 
-void unite(int x, int y) {
-    x = root(x);
-    y = root(y);
-    if (x == y) return;
-
-    if (rankb[x] < rankb[y])
-        parent[x] = y;
-    else {
-        parent[y] = x;
-        if (rankb[x] == rankb[y]) rankb[x]++;
-    }
-}
-
-ll N, Q;
-
-int main() {
-    cin >> N >> Q;
-
-    init(N);
-    REP (q, Q) {
-        int p, a, b;
-        cin >> p >> a >> b;
-        if (p == 0) // 合成
-            unite(a, b);
-        else 
-        cout << (same(a, b) ? "Yes" : "No")  << endl;
+    void init(int n) {
+        N = n;
+        parent.resize(0); parent.clear(); parent.resize(n);
+        REP (i, N) parent[i] = i;
+        rank_.resize(0); rank_.clear(); rank_.resize(n);
     }
 
-    return 0;
-}
+public:
+    UnionFind() { init(0); }
+    UnionFind(int n) { init(n); }
+
+    int root(int x) {
+        return (parent[x] == x) ? x : parent[x] = root(parent[x]);
+    }
+
+    bool isSame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    void merge(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return;
+
+        if (rank_[x] < rank_[y])
+            parent[x] = y;
+        else {
+            parent[y] = x;
+            if (rank_[x] == rank_[y]) rank_[x]++;
+        }
+    }
+};
