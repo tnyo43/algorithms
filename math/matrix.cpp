cc5d5710fe2f3c356553c382724f4543c0add37b
tyno43 <tnyo43@gmail.com>
2020-04-30 17:29:21 +0900

    update matrix module

--- math/matrix.cpp
+++ math/matrix.cpp
@@ -1,112 +1,90 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+ 
 typedef long long ll;
-
-template <typename T>
-struct Matrix {
-    typedef vector<T> vi;
-    typedef vector<vi> vvi;
-
-    vvi data;
-    const int MOD = 1e9+7;
-    int N, M;
-    Matrix (int n, int m) {
-        N = n, M = m;
-        data = vvi(N);
-        for (int i = 0; i < N; ++i) {
-            data[i] = vi(M);
-        }
-    }
-
-    Matrix (vvi A) {
+#define REP(i,n) for (ll i = 0; i < n; ++i)
+ 
+template <typename T> struct Matrix {
+private:
+    typedef vector<T> vt;
+    typedef vector<vt> vvt;
+ 
+    void init(vvt A) {
         N = A.size(), M = A[0].size();
-        data = vvi(A.size());
-        for (int i = 0; i < N; ++i) {
-            data[i] = copy(A[i].begin(), A[i].end());
-        }
+        data = vvt(N, vt(M));
+        REP (i,N) copy(A[i].begin(), A[i].end(), data[i].begin());
     }
-
-    Matrix transverse() {
+ 
+public:
+    int N, M;
+    vvt data;
+ 
+    Matrix (int n, int m) { init(vvt(n, vt(m))); }
+    Matrix (vvt A) { init(A); }
+ 
+    Matrix operator~() { // transverse
         Matrix res(M, N);
         for (int j = 0; j < N; ++j) for (int i = 0; i < M; ++i)
             res.data[i][j] = data[j][i];
         return res;
     }
-
-    int rank() {
-        int res = 0;
-        int le = 0;
-        for (int i = 0; i < M; ++i) {
-            bool flag = false;
-            for (int j = le; j < N; ++j) {
-                if (data[j][i] != 0) {
-                    swap(data[j], data[le]);
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag) {
-                for (int j = le+1; j < N; ++j) {
-                    if (data[j][i] != 0) {
-                        for (int l = 0; l < M; ++l) {
-                            data[j][l] ^= data[le][l];
-                        }
-                    }
-                }
-                res++; le++;
-            }
+ 
+    Matrix &operator+= (const Matrix& b) {
+        REP (i, N) REP (j, M) data[i][j] += b.data[i][j];
+        return *this;
+    }
+ 
+    Matrix &operator-= (const Matrix& b) {
+        REP (i, N) REP (j, M) data[i][j] -= b.data[i][j];
+        return *this;
+    }
+ 
+    Matrix &operator*= (const Matrix& b) {
+        int L = b.M;
+        Matrix ret(N, L);
+        REP (i, N) REP (j, L) {
+            REP (k, M) ret.data[i][j] += data[i][k] * b.data[k][j];
         }
-        return res;
+        M = L;
+        data = ret.data;
+        return *this;
     }
-
-    Matrix add (Matrix& b) {
-        Matrix res(N, M);
-        for (int j = 0; j < N; ++j) for (int i = 0; i < M; ++i)
-            res.data[j][i] = (data[j][i] + b.data[j][i]) % MOD;
-        return res;
+ 
+    Matrix &operator*= (const T c) {
+        REP (i, N) REP (j, M) data[i][j] *= c;
+        return *this;
     }
-
-    Matrix sub (Matrix& b) {
-        Matrix res(N, M);
-        for (int j = 0; j < N; ++j) for (int i = 0; i < M; ++i)
-            res.data[j][i] = (data[j][i] - b.data[j][i]) % MOD;
-        return res;
+ 
+    Matrix operator-() const {
+        Matrix ret = Matrix(*this);
+        REP (i, N) REP (j, M) ret.data[i][j] = -ret.data[i][j];
+        return ret;
     }
-
-    Matrix mult (Matrix& b) {
-        int L = b[0].size();
-        Matrix res(N, L);
-        for (int j = 0; j < N; ++j) for (int i = 0; i < L; ++i) {
-            T temp = 0;
-            for (int k = 0; k < M; ++k) 
-                (temp += data[j][k] * data[k][i]) %= MOD;
-            res.data[j][i] = temp;
-        }
-        return res;
-    }
-
-    // べき乗は正方行列のみ
-    Matrix pow(ll k) {
-        Matrix res(N, N);
-        Matrix temp(data);
+ 
+    Matrix operator+(const Matrix& b) const { return Matrix(*this) += b; }
+    Matrix operator-(const Matrix& b) const { return Matrix(*this) -= b; }
+    Matrix operator*(const Matrix& b) const { return Matrix(*this) *= b; }
+    Matrix operator*(const T c) const { return Matrix(*this) *= c; }
+ 
+    Matrix pow(int64_t k) const {
+        Matrix ret(N, N);
+        REP (i, N) ret.data[i][i] = T(1);
+        Matrix tmp(data);
         while (k) {
-            if (k%1) res = res.mult(temp);
-            temp = temp.mult(temp);
+            if (k&1) ret *= tmp;
+            tmp *= tmp;
             k >>= 1;
         }
-        return res;
+        return ret;
+    }
+ 
+    friend ostream &operator<<(ostream &os, const Matrix &p) {
+        os << "[";
+        REP (i, p.N) {
+            os << "[";
+            REP (j, p.M) os << p.data[i][j] << ((j == p.M-1)?"]":",");
+            os << ((i == p.N-1)?"]":",");
+        }
+        return os;
     }
 };
-
-int main() {
-    int N, M;
-    cin >> N >> M;
-    Matrix<ll> A(N, M);
-    for (int j = 0; j < N; ++j) for (int i = 0; i < M; ++i) {
-        cin >> A.data[j][i];
-    }
-    cout << A.rank() << endl;
-}
