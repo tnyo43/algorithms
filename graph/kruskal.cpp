d7a8a1411ff49a796156172b5c1d9de5943b98c1
tyno43 <tnyo43@gmail.com>
2020-05-15 14:13:08 +0900

    add kruskal module

--- graph/kruskal.cpp
+++ graph/kruskal.cpp
@@ -5,82 +5,81 @@
 using namespace std;
 
 typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i,n) for (ll i = 0; i < n; ++i)
+#define FORE(x,xs) for (const auto& x : xs)
 
-#define REP(i, n) for (ll i = 0; i < (n); ++i)
-#define FORE(x, xs) for (auto &x: (xs))
-#define ALL(v) v.begin(), v.end()
-
-const int INF = 1000000009;
-
-const int N_MAX = 1e5;
-const int M_MAX = 2e5;
-
-int parent[N_MAX];
-int rankb[N_MAX];
+struct UnionFind {
+private:
+    int N;
+    vi parent;
+    vi rank_;
  
-void init(int x) {
-    for(int i = 0; i <= x; i++)
-    parent[i] = i,rankb[i] = 0;
-}
- 
-int root(int x) {
-    return parent[x]==x ? x : parent[x]=root(parent[x]);
-}
- 
-bool same(int x, int y) {
-    return root(x) == root(y);
-}
- 
-void unite(int x, int y) {
-    x = root(x);
-    y = root(y);
-    if (x == y) return;
-
-    if (rankb[x] < rankb[y])
-        parent[x] = y;
-    else {
-        parent[y] = x;
-        if (rankb[x] == rankb[y]) rankb[x]++;
+    void init(int n) {
+        N = n;
+        parent.resize(0); parent.clear(); parent.resize(n);
+        REP (i, N) parent[i] = i;
+        rank_.resize(0); rank_.clear(); rank_.resize(n);
     }
-}
-
-struct Edge {
-    int a, b, cost;
-
-    bool operator<(const Edge& o) const {
-        return cost < o.cost;
+ 
+public:
+    UnionFind() { init(0); }
+    UnionFind(int n) { init(n); }
+ 
+    int root(int x) {
+        return (parent[x] == x) ? x : parent[x] = root(parent[x]);
+    }
+ 
+    bool isSame(int x, int y) {
+        return root(x) == root(y);
+    }
+ 
+    void merge(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return;
+ 
+        if (rank_[x] < rank_[y])
+            parent[x] = y;
+        else {
+            parent[y] = x;
+            if (rank_[x] == rank_[y]) rank_[x]++;
+        }
     }
 };
 
-ll N, M;
-vector<Edge> G;
-
-ll kruskal() { // 求めた最小全域木のコストを返す
-    sort(ALL(G));
-    init(N);
-    ll ans = 0;
-
-    FORE (e, G) {
-        if (!same(e.a, e.b)) {
-        unite(e.a, e.b);
-        ans += e.cost;
+template<typename T> struct Kruskal {
+private:
+    int N;
+    vector<tuple<T, int, int>> G;
+ 
+    void init(int n) {
+        N = n;
+        G = vector<tuple<T, int, int>>(0);
+    }
+ 
+public:
+    Kruskal() {}
+    Kruskal(int n) { init(n); }
+ 
+    void add(int u, int v, T w) {
+        G.push_back(make_tuple(w, u, v));
+    }
+ 
+    pair<T, vector<vector<pair<int, T>>>> solve() {
+        sort(ALL(G));
+        UnionFind uf(N);
+        T ret(0);
+        vector<vector<pair<int, T>>> graph(N);
+        FORE (e, G) {
+            int u, v; T w;
+            tie (w, u, v) = e;
+            if (uf.isSame(u, v)) continue;
+            uf.merge(u, v);
+            ret += w;
+            graph[u].push_back(make_pair(v, w));
+            graph[v].push_back(make_pair(u, w));
         }
+        return make_pair(ret, graph);
     }
-
-    return ans;
-}
-
-int main() {
-    cin >> N >> M;
-
-    REP (i, M) {
-        Edge e;
-        cin >> e.a >> e.b >> e.cost;
-        e.a--; e.b--;
-        G.push_back(e);
-    }
-
-    cout << kruskal() << endl;
-
-    return 0;
-}
+};
